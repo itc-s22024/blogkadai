93ca697d7967afce90548dc147dbe7782896eaf9
s22024 <s22024@std.it-college.ac.jp>
2023-02-16 11:51:32 +0000

    c8fin

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,5 +1,13 @@
 import { getPostBySlug } from "lib/api"
+import { extractText } from "lib/extract-text"
+import Meta from "components/meta"
 import Container from "components/container"
+import PostHeader from "components/post-header"
+import Image from "next/image"
+import ConvertBody from "components/convert-body"
+import PostCategories from "components/post-categories"
+import PostBody from "components/post-body"
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "components/two-column"
 
 export default function Schedule({
     title,
@@ -7,10 +15,45 @@ export default function Schedule({
     content,
     eyecatch,
     categories,
+    description,
 }) {
     return (
         <Container>
-            <h1>{title}</h1>
+           <Meta
+                pageTitle={title}
+                pageDesc={description} 
+                pageImg={eyecatch.url}
+                pageImgW={eyecatch.width}
+                pageImgH={eyecatch.height}
+            />
+            <article>
+                <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+                <figure>
+                    <Image
+                        src={eyecatch.url}
+                        alt=""
+                        width={eyecatch.width}
+                        height={eyecatch.height}
+                        sizes="(min-width: 1152px) 1152px, 100vw"
+                        priority
+                        style={{
+                            width: '100%',
+                            height: 'auto',
+                        }}
+                    />
+                </figure>
+                <TwoColumn>
+                    <TwoColumnMain>
+                        <PostBody>
+                            <ConvertBody contentHTML={content} />
+                        </PostBody>
+                    </TwoColumnMain>
+                    <TwoColumnSidebar>
+                        <PostCategories categories={categories} />
+                    </TwoColumnSidebar>
+                </TwoColumn>
+            </article>
         </Container>
     ) 
 }
@@ -20,6 +63,8 @@ export async function getStaticProps() {
     
     const post = await getPostBySlug(slug)
 
+    const description = extractText(post.content)
+
     return{
         props: {
             title: post.title,
@@ -27,6 +72,7 @@ export async function getStaticProps() {
             content: post.content,
             eyecatch: post.eyecatch,
             categories: post.categories,
+            description: description,
         },
     }
 }
